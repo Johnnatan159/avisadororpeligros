1af75dbaf2e7beea7561150c2f8648db27b98dc5
Johnnatan mauricio Montoya Valencia <23923989x@iespoblenou.org>
2020-06-14 18:48:26 +0200

    parte 1

---
+++ app/src/main/java/com/example/avisadororpeligros/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.avisadororpeligros;
+
+import android.Manifest;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    FragmentManager fm = getSupportFragmentManager();
+
+    final Fragment fragment1 = new NotificarFragment();
+    final Fragment fragment2 = new LlistarFragment();
+    final Fragment fragment3 = new MapaFragment();
+    FusedLocationProviderClient mFusedLocationClient;
+
+    Fragment active = fragment1;
+
+    private SharedViewModel model;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = item -> {
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fm.beginTransaction().hide(active).show(fragment1).commit();
+                active = fragment1;
+                return true;
+            case R.id.navigation_llistat:
+                fm.beginTransaction().hide(active).show(fragment2).commit();
+                active = fragment2;
+                return true;
+            case R.id.navigation_mapa:
+                fm.beginTransaction().hide(active).show(fragment3).commit();
+                active = fragment3;
+                return true;
+        }
+        return false;
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView nav = findViewById(R.id.navigation);
+        nav.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        fm.beginTransaction()
+                .add(R.id.fragment_seleccionat, fragment1, "1")
+                .hide(fragment2)
+                .commit();
+
+        fm.beginTransaction()
+                .add(R.id.fragment_seleccionat, fragment2, "2")
+                .hide(fragment2)
+                .commit();
+
+        fm.beginTransaction()
+                .add(R.id.fragment_seleccionat, fragment3, "3")
+                .hide(fragment3)
+                .commit();
+
+        nav.setSelectedItemId(R.id.navigation_home);
+
+        model = ViewModelProviders.of(this).get(SharedViewModel.class);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        model.setFusedLocationClient(mFusedLocationClient);
+
+        model.getCheckPermission().observe(this, s -> checkPermission());
+    }
+
+    void checkPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        } else {
+            model.startTrackingLocation(false);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                // Si es concedeix permís, obté la ubicació,
+                // d'una altra manera, mostra un Toast
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    model.startTrackingLocation(false);
+                } else {
+                    Toast.makeText(this,
+                            "Permís denegat",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/avisadororpeligros/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,24 @@
+package com.example.avisadororpeligros.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+
+import com.example.avisadororpeligros.R;
+
+public class NotificationsFragment extends Fragment {
+
+    private NotificationsViewModel notificationsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
+}
